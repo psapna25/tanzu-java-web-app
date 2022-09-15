50c32e205dd09ea67e2f2d9c4251ee6b4309f489
psapna25 <104568771+psapna25@users.noreply.github.com>
2022-09-15 06:52:52 +0000

    Update HelloController.java

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "GIT location Crosscluster Git source 1.3.0-build.22";
+		return "GIT location Crosscluster Git source 1.3.0-build.22 + reboot";
 	}
 
 }
